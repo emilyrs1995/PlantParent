49a94602ad7d6cbb887f2dbf6b3b89657c0135b3
imskiboski <jeremy.kasten@mykenzie.snhu.edu>
2024-03-08 17:16:17 -0500

    Created PlantRepository.java for Caio

--- Application/src/main/java/com/kenzie/appserver/repositories/PlantRepository.java
+++ Application/src/main/java/com/kenzie/appserver/repositories/PlantRepository.java
@@ -1,9 +1,9 @@
 package com.kenzie.appserver.repositories;
 
-import com.kenzie.appserver.repositories.model.PlantRecord;
+import com.kenzie.appserver.repositories.model.Plant;
 import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.springframework.data.repository.CrudRepository;
 
 @EnableScan
-public interface PlantRepository extends CrudRepository<PlantRecord, String> {
+public interface PlantRepository extends CrudRepository<Plant, String> {
 }
