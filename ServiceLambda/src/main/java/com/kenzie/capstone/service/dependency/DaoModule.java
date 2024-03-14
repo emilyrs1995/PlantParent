55854824c183e71c115069be44a3f00f5c604b41
imskiboski <jeremy.kasten@mykenzie.snhu.edu>
2024-03-14 15:51:20 -0400

    Cleanup

--- ServiceLambda/src/main/java/com/kenzie/capstone/service/dependency/DaoModule.java
+++ ServiceLambda/src/main/java/com/kenzie/capstone/service/dependency/DaoModule.java
@@ -25,7 +25,6 @@ public class DaoModule {
     @Provides
     @Named("CacheClient")
     public CacheClient provideCacheClient() {
-        // Initialize and return an instance of CacheClient
         return new CacheClient(100);
     }
 
