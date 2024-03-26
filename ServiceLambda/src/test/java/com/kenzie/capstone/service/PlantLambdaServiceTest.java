f9df187e5092d513cfc0d2481280fc11e22d514c
emilys95 <emily.siesel@mykenzie.snhu.edu>
2024-03-26 14:28:21 -0400

    Wrote unit tests for the two service classes, all tests pass

--- ServiceLambda/src/test/java/com/kenzie/capstone/service/PlantLambdaServiceTest.java
+++ ServiceLambda/src/test/java/com/kenzie/capstone/service/PlantLambdaServiceTest.java
@@ -2,6 +2,7 @@ package com.kenzie.capstone.service;
 
 import com.kenzie.capstone.service.dao.NonCachingPlantDao;
 import com.kenzie.capstone.service.exceptions.InvalidDataException;
+import com.kenzie.capstone.service.model.GetPlantDetailsResponse;
 import com.kenzie.capstone.service.model.GetPlantListResponse;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -87,5 +88,71 @@ public class PlantLambdaServiceTest {
         // WHEN
         // THEN
         Assertions.assertThrows(InvalidDataException.class, () -> plantLambdaService.getPlantList(plantName));
+    }
+
+    /** ------------------------------------------------------------------------
+     *  PlantListLambdaService.getPlantDetails
+     *  ------------------------------------------------------------------------ **/
+    @Test
+    void getPlantDetails_successful() {
+        // GIVEN
+        String id = "61";
+        String name = "Hogyoku Japanese Maple";
+        String flowerColor = "Reddish-purple";
+        String maintenance = "Low";
+        String careLevel = "Unknown";
+        String growthRate = "Low";
+        String indoor = "false";
+        String hardinessZone = "7 - 7";
+        String wateringBenchmark = "Every 3-4 days";
+        String medicinal = "false";
+        String description = "Hogyoku Japanese Maple (Acer palmatum 'Hogyoku') is an amazing plant species noted for its striking beauty. " +
+                "Its delicate foliage emerges with a golden hue and develops into a vibrant, deep scarlet during the cooler months. " +
+                "Bright red flowers bloom in spring and bring in stunning hues all season long. The foliage turns to yellow, orange, " +
+                "and pink in the fall. Outstanding bark color is a nice addition to the landscape - from cinnamon-red to orange and yellow. " +
+                "The beauty of tiger-striped bark truly stands out in the winter. Hogyoku is easy to grow and low-maintenance. " +
+                "It also does extremely well in containers and makes a stunning accent for any garden.";
+
+        GetPlantDetailsResponse expectedResponse = new GetPlantDetailsResponse(id, name, flowerColor, maintenance, careLevel,
+                growthRate, indoor, hardinessZone, wateringBenchmark, medicinal, description);
+
+        when(nonCachingPlantDao.getPlantDetails(id)).thenReturn(expectedResponse);
+
+        // WHEN
+        GetPlantDetailsResponse actualResponse = plantLambdaService.getPlantDetails(id);
+
+        // THEN
+        Assertions.assertNotNull(actualResponse);
+        Assertions.assertEquals(expectedResponse.getPlantId(), actualResponse.getPlantId());
+        Assertions.assertEquals(expectedResponse.getPlantName(), actualResponse.getPlantName());
+        Assertions.assertEquals(expectedResponse.getFlowerColor(), actualResponse.getFlowerColor());
+        Assertions.assertEquals(expectedResponse.getMaintenance(), actualResponse.getMaintenance());
+        Assertions.assertEquals(expectedResponse.getCareLevel(), actualResponse.getCareLevel());
+        Assertions.assertEquals(expectedResponse.getGrowthRate(), actualResponse.getGrowthRate());
+        Assertions.assertEquals(expectedResponse.getIndoor(), actualResponse.getIndoor());
+        Assertions.assertEquals(expectedResponse.getHardinessZone(), actualResponse.getHardinessZone());
+        Assertions.assertEquals(expectedResponse.getWateringBenchmark(), actualResponse.getWateringBenchmark());
+        Assertions.assertEquals(expectedResponse.getMedicinal(), actualResponse.getMedicinal());
+        Assertions.assertEquals(expectedResponse.getDescription(), actualResponse.getDescription());
+    }
+
+    @Test
+    void getPlantDetails_withNullId_throwsInvalidDataException() {
+        // GIVEN
+        String id = null;
+
+        // WHEN
+        // THEN
+        Assertions.assertThrows(InvalidDataException.class, () -> plantLambdaService.getPlantDetails(id));
+    }
+
+    @Test
+    void getPlantDetails_withEmptyId_throwsInvalidDataException() {
+        // GIVEN
+        String id = "";
+
+        // WHEN
+        // THEN
+        Assertions.assertThrows(InvalidDataException.class, () -> plantLambdaService.getPlantDetails(id));
     }
 }
