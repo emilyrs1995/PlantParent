251f4b5235ab66d4d6fd931753e00a339906dc16
imskiboski <jeremy.kasten@mykenzie.snhu.edu>
2024-04-10 18:46:24 -0400

    Adding sizing for text and maintained by on home screen

--- Frontend/react-app/app/page.tsx
+++ Frontend/react-app/app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-12 bg-homepage bg-center">
         <Navbar/>
+        <div class="text-sm absolute inset-x-2 bottom-0 h-8 ">Maintained by: Emily, Molly, Jeremy, and Caio</div>
     </main>
   );
 }
